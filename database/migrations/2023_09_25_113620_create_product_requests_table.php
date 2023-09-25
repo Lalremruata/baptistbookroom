7539b23e812159d72cfddd67705232b5ead3cb9d
vagrant <vagrant@homestead>
2023-09-25 12:23:13 +0000

    product_request migration added

---
+++ database/migrations/2023_09_25_113620_create_product_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_requests', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity_requested');
+            $table->date('request_date');
+            $table->enum('status',['pending','approved','rejected']);
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('branch_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_requests');
+    }
+};
