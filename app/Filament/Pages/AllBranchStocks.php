138a2986eecd9a820d6f7c36c5da57fb91a2df93
vagrant <vagrant@homestead>
2024-02-16 12:25:43 +0000

    allbranchstock updated

--- app/Filament/Pages/AllBranchStocks.php
+++ app/Filament/Pages/AllBranchStocks.php
@@ -2,16 +2,150 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\Category;
 use App\Models\Item;
+use App\Models\Sale;
 use App\Models\SubCategory;
+use Filament\Actions\Action;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Page;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
 
-
-class AllBranchStocks extends Page
+class AllBranchStocks extends Page implements HasForms
 {
-
+    use InteractsWithForms;
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
-
+    protected static ?string $navigationGroup = 'Stocks';
     protected static string $view = 'filament.pages.all-branch-stocks';
+    public ?array $data = [];
+    public $sales = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                ->schema([
+                    TextInput::make('barcode')
+                    ->afterStateUpdated(function(callable $set,Get $get){
+                        $barcode = $get('barcode');
+                        $item = Item::where('barcode', $barcode)
+                        ->first();
+                        if($item)
+                        {
+                            $set('item_id', $item->id);
+                        }
+
+                    })
+                    ->autofocus()
+                    ->live()
+                    ->required()
+                    ->dehydrated(),
+                Select::make('category_id')
+                    ->label('Category')
+                    ->searchable()
+                    ->options(Category::query()->pluck('category_name', 'id'))
+                    ->reactive()
+                    ->afterStateUpdated(fn(callable $set)=>$set('sub_category_id', null))
+                    ->required(),
+                Select::make('sub_category_id')
+                    ->label('Sub Categoty')
+                    ->searchable()
+                    ->options(function(callable $get){
+                        $category= Category::find($get('category_id'));
+                        if(!$category){
+                            return null;
+                        }
+                        return $category->subcategories->pluck('subcategory_name','id');
+                    })
+                    ->reactive()
+                    ->required(),
+                    Select::make('item_id')
+                        ->label('Item')
+                            ->options(function(callable $get){
+                                $subCategory= SubCategory::find($get('sub_category_id'));
+                                $item= Item::find($get('barcode'));
+                                if(!$subCategory && $get('barcode')){
+                                    return (Item::query()->pluck('item_name', 'id'));
+                                    // return null;
+                                }
+                                elseif(!$subCategory && !$item){
+                                    // return (Item::query()->pluck('item_name', 'id'));
+                                    return null;
+                                }
+                                return $subCategory->items->pluck('item_name','id');
+                            })
+                            ->afterStateUpdated(fn(callable $set,Get $get)=>$set('barcode',Item::query()
+                                ->where('id', $get('item_id'))->pluck('barcode')->first()))
+                            ->reactive()
+                            ->searchable()
+                            ->required(),
+                ])
+                ->columns(4)
+                ->compact()
+                ->collapsible(),
+
+            ])
+            ->statePath('data');
+    }
+    protected function getFormActions(): array
+    {
+        return [
+            Action::make('showTable')
+                ->label(__('Apply Filter'))
+                ->submit('showTable'),
+        ];
+    }
+
+    public function showTable(): void
+    {
+        $data = $this->form->getState();
+        $sales = Sale::when($data['item_id'], function ($query) use ($data) {
+            $query->whereHas('branchStock.mainStock.item', function ($itemQuery) use ($data) {
+                $itemQuery->where('id', $data['item_id']);
+            });
+        })->get();
+        $this->sales = $sales;
+        // $columns = [
+        //     TextColumn::make('item_name')
+        //         ->label('Item Name')
+        //         ->sortable(),
+        //     TextColumn::make('quantity')
+        //         ->label('Quantity')
+        //         ->sortable(),
+        // ];
+        // return $table
+        // ->columns($columns)
+        // ->rows(function (callable $get) use ($data) {
+        //     // Query your data based on filters
+        //     // ... (update filter conditions based on $data)
+        //     $items = Sale::when($data['item_id'], function ($query) {
+        //         $query->whereHas('branchStock.mainStock.item', function ($itemQuery) {
+        //             $itemQuery->where('id', $this->itemId);
+        //         });
+        //     })->get();
+
+        //     return $items->map(function ($item) use ($get) {
+        //         return [
+        //             'item_name' => $item->item_name,
+        //             'quantity' => $item->stock->quantity, // Assuming you have a "stock" relationship
+        //             'is_available' => $item->stock->is_available,
+        //         ];
+        //     });
+        // });
+    }
 
 }
--- resources/views/filament/pages/all-branch-stocks.blade.php
+++ resources/views/filament/pages/all-branch-stocks.blade.php
@@ -1,5 +1,48 @@
 <x-filament-panels::page>
-    <livewire:SaleFilterComponent />
+    {{--<livewire:SaleFilterComponent />--}}
+    <x-filament::section>
+        <x-filament-panels::form wire:submit="showTable">
+            {{ $this->form }}
+            <x-filament-panels::form.actions
+                :actions="$this->getFormActions()"
+            />
+        </x-filament-panels::form>
+    </x-filament::section>
+    <x-filament::section>
+        {{--@if($sales->count() > 0)--}}
+        <div class="flex">
+            @foreach($sales as $sale)
+            <div class="inline-block mx-4">
+            <table class="w-full border border-collapse">
+                <thead>
+                    <tr>
+                        <th colspan="4" class="p-2 border">{{ $sale->branch->branch_name }}</th>
+                    </tr>
+                    <tr>
+                        <th class="p-2 border">Quantity</th>
+                        <th class="p-2 border">Total Amount</th>
+                        <th class="p-2 border">Branch Stock Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr>
+                            <td class="p-2 border">{{ $sale->quantity }}</td>
+                            <td class="p-2 border">{{ $sale->total_amount }}</td>
+                            <!-- Replace with the actual branch stock data -->
+                            <td class="p-2 border">{{ $sale->branchStock->quantity }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            </div>
+        </div>
+        {{--
+        @elseif($itemId)
+        <p>No sales found.</p>
+        @endif
+        --}}
+    </x-filament::section>
+    <x-filament-actions::modals />
 </x-filament-panels::page>
 
 
